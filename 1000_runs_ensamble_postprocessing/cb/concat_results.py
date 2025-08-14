2ce417b711a81644c665b6ead2fbe8b8aa20701b
jroblar <jroblar98@gmail.com>
2025-08-14 10:24:37 -0600

    fix: update output file path to use RUN_RAW_DATA_DIR_PATH for combined CSV results

--- 1000_runs_ensamble_postprocessing/cb/concat_results.py
+++ 1000_runs_ensamble_postprocessing/cb/concat_results.py
@@ -8,6 +8,9 @@ PARENT_DIR_PATH = os.path.dirname(SCRIPT_DIR_PATH)
 CB_RESULTS_DIR_PATH = os.path.join(SCRIPT_DIR_PATH, "cb_results")
 CB_LA_RESULTS_DIR_PATH = os.path.join(CB_RESULTS_DIR_PATH, data_id)
 DECOMPOSITION_OUTPUT_DIR_PATH = os.path.join(PARENT_DIR_PATH, "output")
+DATA_DIR_PATH = os.path.join(PARENT_DIR_PATH, "data")
+ENSEMBLE_DIR_PATH = os.path.join(DATA_DIR_PATH, "ensemble_data")
+RUN_RAW_DATA_DIR_PATH = os.path.join(ENSEMBLE_DIR_PATH, f"sisepuede_summary_results_run_sisepuede_run_{data_id}")
 
 # --- find all CSV files in the folder ---
 csv_files = [f for f in os.listdir(CB_LA_RESULTS_DIR_PATH) if f.lower().endswith(".csv")]
@@ -24,7 +27,7 @@ for file in csv_files:
 combined_df = pd.concat(df_list, ignore_index=True)
 
 # Optional: save to a single CSV
-output_file = os.path.join(DECOMPOSITION_OUTPUT_DIR_PATH, f"combined_cb_results_{data_id}.csv")
+output_file = os.path.join(RUN_RAW_DATA_DIR_PATH, f"combined_cb_results_{data_id}.csv")
 print(f"Shape of combined DataFrame: {combined_df.shape}")
 print(f"Null values in the combined DataFrame: {combined_df.isnull().sum()}")
 combined_df.to_csv(output_file, index=False)
