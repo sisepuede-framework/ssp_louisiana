794aeedba0aa408157888a654a1664fa322b8028
jroblar <jroblar98@gmail.com>
2025-08-27 16:44:56 -0600

    Refactor directory paths and update run_id for consistency in data processing scripts

--- 1000_runs_ensamble_postprocessing/parse_experiment_in_individual_files.py
+++ 1000_runs_ensamble_postprocessing/parse_experiment_in_individual_files.py
@@ -1,3 +1,4 @@
+# First step of decomposition pipeline
 # This script parses a large CSV file containing simulation data and splits it into individual files based on unique primary IDs.
 # Each individual file is saved in a directory named after the run ID.
 # It ensures that the necessary directories exist and handles the creation of output files.
@@ -12,7 +13,7 @@ DATA_DIR_PATH = os.path.join(SCRIPT_DIR_PATH, "data")
 ENSEMBLE_DIR_PATH = os.path.join(DATA_DIR_PATH, "ensemble_data")
 
 
-run_id = "2025-08-17T22;36;58.136929"
+run_id = "2025-08-27T11;02;10.572056"
 RUN_ENSEMBLE_DIR_PATH = os.path.join(ENSEMBLE_DIR_PATH, f"sisepuede_summary_results_run_sisepuede_run_{run_id}")
 file_name = "WIDE_INPUTS_OUTPUTS.csv"
 full_path = os.path.join(RUN_ENSEMBLE_DIR_PATH, file_name)
@@ -30,10 +31,13 @@ os.makedirs(OUTPUT_DIR_PATH, exist_ok=True)
 # Create a directory with the same name as the run_id
 OUTPUT_DIR_PATH = os.path.join(OUTPUT_DIR_PATH, run_id)
 os.makedirs(OUTPUT_DIR_PATH, exist_ok=True)
+print(f"Output directory created: {OUTPUT_DIR_PATH}")
 
 # Split and save
 for i, pid in enumerate(all_ids, 1):   # start numbering at 1
     pivot = full_sim[full_sim["primary_id"] == pid]
     out_path = os.path.join(OUTPUT_DIR_PATH, f"{i}.csv")
     pivot.to_csv(out_path, index=False)
-    print(f"Saved: {out_path}")
+    # print(f"Saved: {out_path}")
+    
+print(f"Saved {len(all_ids)} files in: {OUTPUT_DIR_PATH}")
